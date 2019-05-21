9f50c7dfa5fe6b3fc3ac5b63de3ee6a651c65b7a
Colton Travers <colton.travers@parkmobile.io>
2019-05-21 13:39:27 -0400

    Remove mobx from component

--- src/containers/components/slide/compact-slide/CompactSlide.js
+++ src/containers/components/slide/compact-slide/CompactSlide.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { inject, observer } from "mobx-react";
 import CompactSlideView from "./compact-slide-view/CompactSlideView";
 import { Slide } from "./CompactSlide.styles";
 
 class CompactSlide extends Component {
     render() {
-        const {
-            store: { slides },
-            slideNumber
-        } = this.props;
+        const { slideNumber } = this.props;
 
         return (
             <Slide>
@@ -20,8 +16,7 @@ class CompactSlide extends Component {
 }
 
 CompactSlide.propTypes = {
-    store: PropTypes.object.isRequired,
     slideNumber: PropTypes.number.isRequired
 };
 
-export default inject("store")(observer(CompactSlide));
+export default CompactSlide;
